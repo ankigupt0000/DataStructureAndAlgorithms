32af542ff6ac156a6b7eddc074e6ee6a6c677c07
Ankit Gupta <ankigupt@gmail.com>
2018-08-10 13:26:15 +0530

    correcting doxygen comments

--- forward_list/forward_list.h
+++ forward_list/forward_list.h
@@ -1,14 +1,14 @@
-/*!
- * @author Ankit Gupta
- * @version 0.1
- * @date 10 Aug 2018
- * @brief Implementation of forward_list
- * @details Header file as interface for forward_list
+/**
+ * \author Ankit Gupta
+ * \version 0.1
+ * \date 10 Aug 2018
+ * \brief Implementation of forward_list
+ * \details Header file as interface for forward_list
  */
 /**
- * @typedef node_t NODE includes a generic data element and pointer to next node
+ * \typedef node_t NODE includes a generic data element and pointer to next node
  */
 typedef struct node_t{
-   void* data;           //!< @var data pointer to void generic data holder
-   NODE* next_node;      //!< @var next_node pointer to next node
+   void* data;           /**< \var data pointer to void generic data holder */
+   NODE* next_node;      /**< \var next_node pointer to next node */
 } NODE;
