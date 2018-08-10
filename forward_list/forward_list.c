5d1512a7b0a4d3e556945c84dceb03c9d25af50e
Ankit Gupta <ankigupt@gmail.com>
2018-08-10 12:20:56 +0530

    adding header and c file for forward list

---
+++ forward_list/forward_list.c
@@ -0,0 +1,8 @@
+/*!
+ * @author Ankit Gupta
+ * @version 0.1
+ * @date 10 Aug 2018
+ * @brief Implementation of forward_list
+ * @details C version of STL forward list
+ */
+
---
+++ forward_list/forward_list.h
@@ -0,0 +1,14 @@
+/*!
+ * @author Ankit Gupta
+ * @version 0.1
+ * @date 10 Aug 2018
+ * @brief Implementation of forward_list
+ * @details Header file as interface for forward_list
+ */
+/*!
+ * \typedef NODE includes a generic data element and pointer to next node
+ */
+typedef struct node_t{
+   void* data;           //!> \var pointer to void generic data holder
+   NODE* next_node;      //!> \var pointer to next node
+} NODE;
