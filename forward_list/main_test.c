6cd40798900f870fa53908e6282c24682e47ef09
Ankit Gupta <ankigupt@gmail.com>
2018-08-11 01:06:43 -0400

    cleanup code

--- forward_list/main_test.c
+++ forward_list/main_test.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "forward_list.h"
+void insert_list()
+{
+    int i=0;
+    int val[]={10,20,30};
+    int* value;
+    /* test push front */
+    for(i=0;i<3;i++)
+    {
+        value=(int*)malloc(sizeof(int));
+        *value=val[i];
+        push_front((void*)value);
+    }
+}
+void clean_list()
+{
+    NODE **start = before_begin();
+    NODE *itr=NULL,*oldnode=NULL;
+    for(itr=*start;itr != NULL;)
+    {
+        free(itr-> data);
+        oldnode=itr;
+        itr=itr -> next_node;
+        free(oldnode);
+        oldnode=NULL;
+    }
+}
 int main()
 {
-    int itr=0;
     NODE** start = before_begin();
+    NODE* itr=NULL;
     int* dataptr=NULL;
-
-    int val[]={10,20,30};
-    /* test push front */
-    for(itr=0;itr<3;itr++)
-    {
-        push_front((void*)&(val)+(sizeof(int)*itr));
-    }
-
+    insert_list();
     /* Print all emement values */
-    for(NODE* itr=*start;itr != NULL; itr = itr -> next_node)
+    for(itr=*start;itr != NULL; itr = itr -> next_node)
     {
         dataptr=(int*)itr->data;
         printf("%d\n",*dataptr);
     }
+    clean_list();
 }
